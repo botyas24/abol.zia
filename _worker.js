e915d58b9f6ec64ad640267ed67071bdefdeddd2
botyas24 <163675231+botyas24@users.noreply.github.com>
2024-04-27 10:05:45 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'c4973530-06b0-455b-bc04-809521912bf0';
+let userID = 'bcd0e474-d12c-4b2c-a1d0-bcea90ad27b1';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
